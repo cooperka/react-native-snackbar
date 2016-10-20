89b7e582435487a9e22184b0b2051b0ae6b6ede0
Kevin Cooper <cooperka92@gmail.com>
2016-10-20 14:37:31 -0400

    Accept title and duration as params on Android, fix errors

--- android/src/main/java/com/azendoo/reactnativesnackbar/SnackbarModule.java
+++ android/src/main/java/com/azendoo/reactnativesnackbar/SnackbarModule.java
@@ -2,9 +2,13 @@ package com.azendoo.reactnativesnackbar;
 
 import android.support.design.widget.Snackbar;
 
+import android.view.View;
+
+import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
 
 public class SnackbarModule extends ReactContextBaseJavaModule{
 
@@ -20,8 +24,17 @@ public class SnackbarModule extends ReactContextBaseJavaModule{
     }
 
     @ReactMethod
-    public void show() {
-        // TODO: Check getCurrentActivity
-        Snackbar.make(getCurrentActivity().getWindow().getDecorView().getRootView(), "Hello", Snackbar.LENGTH_SHORT).show();
+    public void show(ReadableMap options, Callback callback) {
+        View view = getCurrentActivity().findViewById(android.R.id.content);
+
+        if (view == null) return;
+
+        // TODO: Set length properly https://developer.android.com/reference/android/support/design/widget/Snackbar.html
+        String title = options.hasKey("title") ? options.getString("title") : "Hello";
+        int duration = options.hasKey("duration") ? options.getInt("duration") : Snackbar.LENGTH_SHORT;
+
+        Snackbar
+                .make(view, title, duration)
+                .show();
     }
 }
