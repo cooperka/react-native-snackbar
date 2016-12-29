23691242a649d8711c274db215488ae77866b0be
Kevin Cooper <cooperka92@gmail.com>
2016-12-29 17:00:04 -0500

    [Example] Clean up a bit

--- example/index.js
+++ example/index.js
@@ -5,44 +5,50 @@
  */
 
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 import Snackbar from 'react-native-snackbar';
 
 class Example extends Component {
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.title}>
-          React Native Snackbar
+          Snackbar Examples
         </Text>
-        <Text style={styles.button} onPress={() => Snackbar.show({title: 'Hello Wordl!'})}>
+
+        <Text style={styles.button} onPress={() => Snackbar.show({title: 'Hello, World!'})}>
           Simple Snackbar
         </Text>
-        <Text style={styles.button} onPress={() => Snackbar.show({
-          title: 'Hello World! How are you doing today?! Enjoying the sun?',
-          duration: Snackbar.LENGTH_LONG,
-        })}>
-          Snackbar two lines
+
+        <Text
+          style={styles.button}
+          onPress={() => Snackbar.show({
+            title: 'Hello, World! How are you doing today? Enjoying the sun?! This should wrap to two lines.',
+            duration: Snackbar.LENGTH_LONG,
+          })}
+        >
+          Simple Snackbar - two lines
         </Text>
-        <Text style={styles.button} onPress={() => Snackbar.show({
-          title: 'Please agree to this',
-          duration: Snackbar.LENGTH_INDEFINITE,
-          action: {
-            title: 'AGREE',
-            onPress: () => Snackbar.show({title: 'Thank you'}),
-            color: 'green'
-          }
-        })}>
+
+        <Text
+          style={styles.button}
+          onPress={() => Snackbar.show({
+            title: 'Please agree to this.',
+            duration: Snackbar.LENGTH_INDEFINITE,
+            action: {
+              title: 'AGREE',
+              onPress: () => Snackbar.show({ title: 'Thank you!' }),
+              color: 'green',
+            },
+          })}
+        >
           Snackbar with action
         </Text>
       </View>
     );
   }
+
 }
 
 const styles = StyleSheet.create({
