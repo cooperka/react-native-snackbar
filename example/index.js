266cb7d09bfbaff1f0828a54766e0bf06153c8a3
Kevin Cooper <cooperka92@gmail.com>
2017-03-18 14:46:59 -0400

    :package: Move source code into src; don't publish tests

--- example/index.js
+++ example/index.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import { AppRegistry, Text, View } from 'react-native';
 import Snackbar from 'react-native-snackbar';
