7ba734e73f31c8fae56ca20ec8225b1f256d5e59
Kevin Cooper <cooperka92@gmail.com>
2020-12-16 18:12:50 -0800

    Minor tweaks

--- example/src/App.js
+++ example/src/App.js
@@ -30,10 +30,10 @@ class Example extends Component {
           onPress={() => Snackbar.show({
             text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             duration: Snackbar.LENGTH_LONG,
-            numberOfLines: 10,
+            numberOfLines: 5,
           })}
         >
-          <Text style={styles.button}>Simple Snackbar - multiline</Text>
+          <Text style={styles.button}>Simple Snackbar - extra lines</Text>
         </TouchableOpacity>
 
         <TouchableOpacity
