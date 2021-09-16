19428a9226fac175411c4e0c61ca69206b64ecbc
rvasseur31 <raphael.vasseur@ynov.com>
2021-09-16 13:47:40 +0200

    Mock add listener for testing

--- example/src/__tests__/index.test.js
+++ example/src/__tests__/index.test.js
@@ -3,11 +3,13 @@ import React from 'react';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import Snackbar from 'react-native-snackbar';
 
 import App from '../App';
 
 describe('Snackbar example app', () => {
   it('renders without crashing', () => {
+    Snackbar.emitter = { addListener: jest.fn() };
     renderer.create(<App />);
   });
 });
