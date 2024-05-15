655691a0a529f9f5a59318a67884e1f7416b8fab
jakequade <j2k4@pm.me>
2024-05-15 15:49:04 +1000

    add mock for jest testing

---
+++ jest/snackbar-mock.js
@@ -0,0 +1,14 @@
+/**
+ * @format
+ */
+/* eslint-env jest */
+
+const RNSnackbar = {
+  LENGTH_SHORT: 5,
+  LENGTH_LONG: 10,
+  LENGTH_INDEFINITIE: -1,
+  show: jest.fn(),
+  dismiss: jest.fn(),
+};
+
+module.exports = RNSnackbar;
