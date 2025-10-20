0b311cf41d2f5b9cb442450c1238227d40e27c88
Khanh Le Xuan <crazycommander111@gmail.com>
2025-10-21 01:34:07 +0700

    update example using microsoft/react-native-test-app

--- example/index.js
+++ example/index.js
@@ -1,5 +1,9 @@
+/**
+ * @format
+ */
+
 import { AppRegistry } from 'react-native';
+import App from './App';
+import { name as appName } from './app.json';
 
-import App from './src/App';
-
-AppRegistry.registerComponent('SnackbarExample', () => App);
+AppRegistry.registerComponent(appName, () => App);
